e073b0ea0483d0722ce9590d0a8c99b59e36cdf6
Bruno Mendes <brunosouza@alunos.utfpr.edu.br>
2016-03-27 21:59:41 -0300

    Estrutura Heap Inicial

--- Sources/Heap.swift
+++ Sources/Heap.swift
@@ -1,3 +1,16 @@
 public class Heap{
-	
+
+	public struct No{
+		var peso: Int
+		var caracter: Int
+		var esquerda: Int
+		var direita: Int
+	}
+
+	var raiz: Int = -1
+	var vetor = [No]()
+
+	public init(){
+
+	}
 }

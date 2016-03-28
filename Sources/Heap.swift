aaeb43fe3fb39e347c3588021a946dd677c0c7e3
Bruno Mendes <brunosouza@alunos.utfpr.edu.br>
2016-03-27 22:21:04 -0300

    Implementado minHeapify

--- Sources/Heap.swift
+++ Sources/Heap.swift
@@ -22,11 +22,38 @@ public class Heap{
 		return indice/2
 	}
 
-	private func indiceDireita(indice: Int) -> Int{
+	private func indiceEsquerda(indice: Int) -> Int{
 		return indice * 2
 	}
 
-	private func indiceEsquerda(indice: Int) -> Int{
+	private func indiceDireita(indice: Int) -> Int{
 		return (indice * 2) + 1
+	}
+
+	private func ehFolha(indice: Int) -> Bool{
+		if (indice >= (vetor.count / 2) && indice < vetor.count){
+			return true;
+		}
+		return false;
+	}
+
+	private func troca(indice1: Int, indice2: Int){
+		let auxiliar = vetor[indice1]
+		vetor[indice1] = vetor[indice2]
+		vetor[indice2] = auxiliar
+	}
+
+	private func minHeapify(indice: Int){
+		if (!ehFolha(indice)){ 
+			if (vetor[indice].peso > vetor[indiceEsquerda(indice)].peso  || vetor[indice].peso > vetor[indiceDireita(indice)].peso) {
+				if (vetor[indiceEsquerda(indice)].peso < vetor[indiceDireita(indice)].peso) {
+					troca(indice, indice2: indiceEsquerda(indice));
+					minHeapify(indiceEsquerda(indice));
+				} else {
+					troca(indice, indice2: indiceDireita(indice));
+					minHeapify(indiceDireita(indice));
+				}
+			}
+		}
 	}
 }

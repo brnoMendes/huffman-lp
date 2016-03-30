94c20cff7aff8d0615941e5ecf27c1a439f2dd4a
Bruno Mendes <brunosouza@alunos.utfpr.edu.br>
2016-03-30 05:02:59 -0300

    Métodos do Compactador

--- Sources/Huffman.swift
+++ Sources/Huffman.swift
@@ -1,3 +1,17 @@
+import Foundation
+
 public class Huffman{
-	
+
+	public func Huffman(opcao: Bool, diretorioEntrada: String, diretorioSaida: String) {
+		let arquivo = ManipuladorArquivo()
+		if(opcao == true){
+			let compactador = Compactador()
+			if let texto = arquivo.lerArquivoASCII(diretorioEntrada){
+				compactador.compactar(texto)
+				arquivo.escreverArquivoBinario(diretorioSaida, dados: compactador.dado)
+			}
+		} else {
+			let descompactador = Descompactador()
+		}
+	}
 }
---
+++ Sources/main.swift
@@ -0,0 +1,4 @@
+let huffman = Huffman()
+huffman.Huffman(true, diretorioEntrada: "/home/bruno/arquivo.txt", diretorioSaida: "/home/bruno/binario")
+
+huffman.Huffman(false, diretorioEntrada: "/home/bruno/binario", diretorioSaida: "/home/bruno/arquivo2.txt")

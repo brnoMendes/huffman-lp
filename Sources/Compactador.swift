ba237e490ddd49ebe41c2d03f6dadd8ac1d11579
Bruno Mendes <brunosouza@alunos.utfpr.edu.br>
2016-04-03 23:36:03 -0300

    Modularização e Padronização e Comentários Compactador.swift

--- Sources/Compactador.swift
+++ Sources/Compactador.swift
@@ -9,28 +9,36 @@ public class Compactador {
 	public func compactar(dado: String) -> NSData {
 		var frequencia = [Int](count: 128, repeatedValue: 0)
 
-		// Contar a frequencia de cada caracter no texto.
-		for character in String(dado).utf8 { 
-			frequencia[Int(character)] += 1
+		// Contar a frequencia de cada caracter no texto para o vetor de Frequencia.
+		for caracter in String(dado).utf8 { 
+			frequencia[Int(caracter)] += 1
 		}
 
+		// Grava o vetor de Frequencia no dadoCompactado.
 		for i in 0...127 {
 			var byte = frequencia[i]
 			dadoCompactado.appendBytes(&byte, length: 8)
 		}
 
+		// Cria a Arvore de acordo com o vetor de Frequencia.
 		let arvore = Arvore()
 		let raiz = arvore.constroiArvore(frequencia)
 
+		// Para cada caracter do texto, gravar sua nova representação no dadoCompactado.
 		for caracter in String(dado).utf8 { 
 			procuraFolha(raiz, caracter: UInt8(caracter))
 		}
 
+		// Caso o último byte não tenha sido completado, insere 0's.
 		escreveResto()
 
 		return dadoCompactado
 	}
 
+	/*
+	 * Percorre toda a árvore e encontra todos os nós folhas, para cada nó folha, chama
+	 * o método escreveBits passando o nó.
+	 */
 	private func procuraFolha(no: No<Int>, caracter: UInt8){
 		if(no.direita == nil && no.esquerda == nil && no.caracter == caracter){
 			escreverBits(no, filho: nil)
@@ -44,6 +52,11 @@ public class Compactador {
 		}
 	}
 
+	/* 
+	 * Chama-se a partir de um nó folha o seu pai, consequentemente o pai do pai, até
+	 * chegar a raiz. Depois volta escrevendo 0 ou 1, para um arco da esquerda ou da
+	 * direita.
+	 */
 	private func escreverBits(no: No<Int>, filho: No<Int>?){
 		if(no.pai != nil){
 			escreverBits(no.pai!, filho: no)
@@ -57,15 +70,25 @@ public class Compactador {
 		}
 	}
 
+	/*
+	 * Escreve um novo bit 0 ou 1 no byteSaida. Se o byteSaida estiver completo,
+	 * ou seja, com 8 bits, adiciona ao dadoCompactado.
+	 */
 	private func escreveBit(bit: Bool) {
 		if contadorBit == 8 {
 			dadoCompactado.appendBytes(&byteSaida, length: 1)
 			contadorBit = 0
 		}
-		byteSaida = (byteSaida << 1) | (bit ? 1 : 0)
+		bit = bit ? 1 : 0
+		byteSaida = (byteSaida << 1) | bit
 		contadorBit += 1
 	}
 
+	/*
+	 * Caso o contadorBit seja maior que 0, ou seja, bits ainda restantes para serem
+	 * inseridos no dadoCompactado: Se o byteSaida contem menos de 8 bits, adiciona-se 
+	 * 0's a sua direita. Quando completo, é adicionado ao dadoCompactado.
+	 */
 	private func escreveResto() {
 		if contadorBit > 0 {
 			if contadorBit < 8 {

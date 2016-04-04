cda59e6460e7b0337c2bdb3ace662126db63479e
Bruno Mendes <brunosouza@alunos.utfpr.edu.br>
2016-04-03 23:50:12 -0300

    Comentários Descompactador.swift

--- Sources/Decompactador.swift
+++ Sources/Decompactador.swift
@@ -6,19 +6,27 @@ public class Descompactador {
 		
 		var frequencia = [Int](count: 128, repeatedValue: 0)
 
+		// Cria um ManipuladorBit para o dado compactado. E Lê o vetor de frequencia.
 		let leitorBit = ManipuladorBit(dado: dado)
 		for i in 0...127 {
 			frequencia[i] = leInt(leitorBit)
 		}
 
+		// Remonta a árvore com o vetor de frequencia lido.
 		let arvore = Arvore()
 		let raiz = arvore.constroiArvore(frequencia)
 
 		let quantidadeCaracteres = raiz.peso!
 
+		// Remontar o texto na váriavel string, a partir da arvore remontada e da leitura
+		// de bits do arquivo compactado.
+		// For de 0 até quantidade de caracteres presente no texto original.
 		var string: String = ""
 		for _ in 0...(quantidadeCaracteres - 1){
 			var caracter = raiz;
+
+			// Percorre a árvore a partir da raiz, com 1 para direita e 0 para esquerda,
+			// até encontrar um nó folha.
 			while caracter.direita != nil && caracter.esquerda != nil {
 				if leitorBit.leBit() {
 					caracter = caracter.direita!
@@ -26,23 +34,33 @@ public class Descompactador {
 					caracter = caracter.esquerda!
 				}
 			}
+
+			// Adiciona o caracter do nó folha encontrado na string.
 			string.append(UnicodeScalar(caracter.caracter!))
 		}
+
 		return string
 	}
 
+	/*
+	 * Lê o proximo Int no dadoCompactado. 
+	 */
 	private func leInt(leitorBit: ManipuladorBit) -> Int {
 		var int = 0
 		var shift = 0;
+		// For de 0 a 7, ou seja, lê 8 bytes para o Int.
 		for _ in 0...7 {
 			var byte: UInt8 = 0
 			var mascara: UInt8 = 0x80
+
+			// For de 0 a 7, 8 bits para um byte.
 			for _ in 0...7 {
 				if leitorBit.leBit() {
 					byte = byte | mascara
 				}
 				mascara = mascara >> 1
 			}
+
 			int = int | (Int(byte) << shift)
 			shift += 8
 		}

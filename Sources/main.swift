ccf8223061ee0a4871e75f45d0e2f791793050c7
Bruno Mendes <brunosouza@alunos.utfpr.edu.br>
2016-04-18 08:22:29 -0300

    Modificações main.swift

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,10 +1,7 @@
 let args = [String](Process.arguments)
-
-let huffman = Huffman()
-
 var entrada = args[2]
 var saida = args[3]
-
 var opcao = Int(args[1]) == 0 ? false : true
 
+let huffman = Huffman()
 huffman.Huffman(opcao, diretorioEntrada: entrada, diretorioSaida: saida)
